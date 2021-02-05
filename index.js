ac513bf90a0304515bac6ad2bbc7d54f9d632966
Emma Konrad <emma.konrad92@gmail.com>
2021-02-05 17:22:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function setBestCustomer() {
   bestCustomer = 'not bob';
 }
 
-function overwriteBestCustomer(bestCustomer) {
+function overwriteBestCustomer() {
   bestCustomer = 'maybe bob';
   return bestCustomer;
 }
