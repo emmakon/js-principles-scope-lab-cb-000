52542546ce2fa1d160ae89fc8d00f17dc76c5675
Emma Konrad <emma.konrad92@gmail.com>
2021-02-05 17:14:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var customerName = 'bob';
 
 function upperCaseCustomerName(customerName) {
   customerName.toUpperCase();
-  return customerName
+  return customerName;
 }
 
 function setBestCustomer() {
